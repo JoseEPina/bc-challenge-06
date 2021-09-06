6c15d053996825e3997eac4e6de7dd563ce53dc1
JoseEPina <jose.edpina@gmail.com>
2021-09-05 19:14:06 -0600

    Enabled API process for data retrival & city search enabled

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# bc-challenge-06
+# bc-challenge-06 / Weather Dashboard
+
+This is a simple weather dashboard application that allows a user to retrieve weather data for cities. This app runs in the browser and features dynamically updated HTML and CSS.
+
+## User Story
+
+```
+- AS A traveler
+- I WANT to see the weather outlook for multiple cities
+- SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+- GIVEN a weather dashboard with form inputs
+- WHEN I search for a city
+- THEN I am presented with current and future conditions for that city and that city is added to the search history
+- WHEN I view current weather conditions for that city
+- THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+- WHEN I view the UV index
+- THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+- WHEN I view future weather conditions for that city
+- THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+- WHEN I click on a city in the search history
+- THEN I am again presented with current and future conditions for that city
+```
+
+The following animation demonstrates the application functionality:
+
+<!-- ![code quiz](./assets/mock-up/mock-up.gif) -->
+
+### Review
+
+-  The URL of the functional, deployed application:
+-  https://joseepina.github.io/bc-challenge-06/
+
+-  The URL of the GitHub repository:
+-  https://github.com/JoseEPina/bc-challenge-06
---
+++ assets/js/script.js
@@ -0,0 +1,157 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#cityname");
+var cityContainterEL = document.querySelector("#city-container");
+var citySearchTerm = document.querySelector("#city-search-term");
+
+var city = "";
+var state = "";
+var country = "";
+
+var formSubmitHandler = function (event) {
+   event.preventDefault(); // To disable <button> "submit/refresh page" behaviour
+   // get value from input element
+   var userCity = cityInputEl.value.trim().split(",");
+   city = userCity[0].trim();
+   state = userCity[1].trim();
+   country = userCity[2].trim();
+
+   // if city name matches city in Repo then run our function to
+   // retrieve the data
+   if (userCity) {
+      console.log(city, state, country);
+      getCityData(city, state, country);
+      // Then, run this code to clear the form and remove the <input> element's value.
+      cityInputEl.value = "";
+   } else {
+      window.alert("Please enter a City Name");
+   }
+};
+
+var displayWeatherDOM = function (weatherForecast) {
+   cityContainterEL.textContent = "";
+   // citySearchTerm.textContent = 
+   
+   for (var i = 0; i < weatherForecast.length; i++)
+   {
+   
+   }
+};
+
+var getOneDayData = function (groupData, index) {
+   // console.log(groupData, index);
+   const CURRENT_DAY = 0;
+   const MODERATE = 3.0;
+   const HIGH = 6.0;
+   const VERY_HIGH = 8.0;
+   const EXTREME = 11.0;
+
+   var weatherData = {
+      city: city,
+      state: state,
+      country: country,
+      date: "",
+      icon: "",
+      temp: "",
+      humidity: "",
+      windSpeed: "",
+      uvIndex: "",
+      uvColor: "",
+      description: "",
+   };
+   if (index === CURRENT_DAY) {
+      weatherData.date = new Date(groupData.dt * 1000);
+      weatherData.icon = groupData.weather[0].icon;
+      weatherData.temp = groupData.temp;
+      weatherData.humidity = groupData.humidity;
+      weatherData.windSpeed = groupData.wind_speed;
+      weatherData.uvIndex = parseFloat(groupData.uvi);
+      weatherData.description = groupData.weather[0].description;
+   } else {
+      weatherData.date = new Date(groupData[index].dt * 1000);
+      weatherData.icon = groupData[index].weather[0].icon;
+      weatherData.temp = groupData[index].temp;
+      weatherData.humidity = groupData[index].humidity;
+      weatherData.windSpeed = groupData[index].wind_speed;
+      weatherData.uvIndex = parseFloat(groupData[index].uvi);
+      weatherData.description = groupData[index].weather[0].description;
+   }
+   if (weatherData.uvIndex < MODERATE) {
+      weatherData.uvColor = "green";
+   } else if (weatherData.uvIndex < HIGH) {
+      weatherData.uvColor = "orange";
+   } else if (weatherData.uvIndex < VERY_HIGH) {
+      weatherData.uvColor = "red";
+   } else if (weatherData.uvIndex < EXTREME) {
+      weatherData.uvColor = "purple";
+   }
+   return weatherData;
+};
+
+var getCityData = function (city, state, country) {
+   var apiCity =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "," +
+      state +
+      "," +
+      country +
+      "&units=imperial&appid=58f880309006154d3fa9ee5e7617aa87";
+   // Make a request to the url
+   fetch(apiCity)
+      .then(function (response) {
+         console.log(city);
+         if (response.ok) {
+            response.json().then(function (data) {
+               console.log(data);
+               latitude = data.coord.lat;
+               longitude = data.coord.lon;
+               // displayWeather(data, city);
+               getWeatherData(latitude, longitude);
+            });
+         } else {
+            throw new Error(response.statusText); //
+         }
+      })
+      .catch(function (error) {
+         // '.catch()' method is being chained onto the end
+         // to verify connection to API
+         window.alert("OpenWeather Server Is Not Responding: ", error);
+      });
+};
+
+// function to retrive
+var getWeatherData = function (latitude, longitude) {
+   var apiOneCall =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&exclude=minutely,hourly,alerts&units=imperial&appid=58f880309006154d3fa9ee5e7617aa87";
+
+   fetch(apiOneCall)
+      .then(function (response) {
+         if (response.ok) {
+            // console.log(response);
+            response.json().then(function (data) {
+               console.log("OneCall Data: ", data);
+               var weatherForecast = [];
+               // Loop to push 5 day forecast to our object array
+               const CURRENT_DAY = 0;
+               const FORECAST_DAYS = 6;
+               weatherForecast.push(getOneDayData(data.current, CURRENT_DAY));
+               for (var forecastDay = 1; forecastDay < FORECAST_DAYS; forecastDay++) {
+                  weatherForecast.push(getOneDayData(data.daily, forecastDay));
+               }
+               console.log("Weather Forecast: ", weatherForecast);
+               displayWeatherDOM(weatherForecast);
+            });
+         } else {
+            throw new Error(response.statusText); //
+         }
+      })
+      .catch(function (error) {
+         window.alert("OpenWeather Server Is Not Responding: ", error);
+      });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
